0ba34bb193a5153e7f995fd7fd891a8d78d89bea
Valik <valikpost8@gmail.com>
2020-05-05 14:59:12 +0300

    05.05.2020 bright animal

--- js/bright.js
+++ js/bright.js
@@ -21,7 +21,18 @@ let scaleCount = 4;
 //     };
 // });
 $(window).on('scroll', function(){
-    if(window.pageYOffset >= studyPosition.top - 400 && scaleCount < 4 && window.pageYOffset <= $('.study_second_mob').offset().top){
+    console.log("window",window.pageYOffset);
+    console.log("studyPosition",studyPosition.top);
+
+    if(window.pageYOffset >= studyPosition.top - 400 && scaleCount > 1.6){
+        console.log('down');
+        count-=50;
+        $(".cloudL").css('left', count);
+        $(".cloudR").css('right', count);
+        scaleCount-=0.4;
+        $(".cloudL, .cloudR").css('transform', 'scale('+scaleCount+')');
+    }
+    else if(window.pageYOffset <= studyPosition.top - 400 && scaleCount < 4){
         console.log('up');
         count+=50;
         $(".cloudL").css('left', count);
@@ -29,12 +40,4 @@ $(window).on('scroll', function(){
         scaleCount+=0.4;
         $(".cloudL, .cloudR").css('transform', 'scale('+scaleCount+')');
     }
-    else if(window.pageYOffset <= $('.study_second_mob').offset().top + 400 && scaleCount > 1.6){
-        console.log('down');
-        count-=50;
-        $(".cloudL").css('left', count);
-        $(".cloudR").css('right', count);
-        scaleCount-=0.4;
-        $(".cloudL, .cloudR").css('transform', 'scale('+scaleCount+')');
-    };
 });
