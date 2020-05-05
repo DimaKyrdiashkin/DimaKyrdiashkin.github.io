c95eef29b5f33d03912de0901cf26dda56019e76
DimaKyrdiashkin <44475715+DimaKyrdiashkin@users.noreply.github.com>
2020-05-05 15:20:33 +0300

    05.05.2020

--- js/bright.js
+++ js/bright.js
@@ -2,26 +2,8 @@ let studyPosition = $('.study').offset();
 let count = 400;
 let scaleCount = 4;
 
-// $(window).bind('mousewheel', function(event){
-//     if(event.originalEvent.wheelDelta >= 0 && $(document).scrollTop() <= studyPosition.top && scaleCount < 4){
-//         console.log('up');
-//         count+=50;
-//         $(".cloudL").css('left', count);
-//         $(".cloudR").css('right', count);
-//         scaleCount+=0.4;
-//         $(".cloudL, .cloudR").css('transform', 'scale('+scaleCount+')');
-//     }
-//     else if(event.originalEvent.wheelDelta <= 0 && $(document).scrollTop() >= studyPosition.top-800 && scaleCount > 1.6){
-//         console.log('down');
-//         count-=50;
-//         $(".cloudL").css('left', count);
-//         $(".cloudR").css('right', count);
-//         scaleCount-=0.4;
-//         $(".cloudL, .cloudR").css('transform', 'scale('+scaleCount+')');
-//     };
-// });
-$(window).on('scroll', function(){
-    if(window.pageYOffset >= studyPosition.top - 400 && scaleCount < 4 && window.pageYOffset <= $('.study_second_mob').offset().top){
+$(window).bind('mousewheel', function(event){
+    if(event.originalEvent.wheelDelta >= 0 && $(document).scrollTop() <= studyPosition.top && scaleCount < 4){
         console.log('up');
         count+=50;
         $(".cloudL").css('left', count);
@@ -29,7 +11,7 @@ $(window).on('scroll', function(){
         scaleCount+=0.4;
         $(".cloudL, .cloudR").css('transform', 'scale('+scaleCount+')');
     }
-    else if(window.pageYOffset <= $('.study_second_mob').offset().top + 400 && scaleCount > 1.6){
+    else if(event.originalEvent.wheelDelta <= 0 && $(document).scrollTop() >= studyPosition.top-800 && scaleCount > 1.6){
         console.log('down');
         count-=50;
         $(".cloudL").css('left', count);
