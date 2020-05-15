48747a2a43d67602bb0c90365422f79b4629b46f
vvalik <ghost0962421083>
2020-05-15 17:57:02 +0300

    15.05.2020 media v3

--- js/say_slider.js
+++ js/say_slider.js
@@ -1,14 +1,43 @@
 "user script"
 
+//
+// $(".say_slider").slick({
+//     dots: true,
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3,
+//     adaptiveHeight: true,
+//     // autoplay:true,
+//     autoplaySpeed:5000,
+//     prevArrow: "<img src='img/home/slider_slick_next.svg' class='prev' alt='1'>",
+//     nextArrow: "<img src='img/home/slider_slick_next.svg' class='next' alt='2'>",
+// });
+if(window.innerWidth <= 991){
+    saySlider(1)
+}
+else{
+    saySlider(3)
+}
+$(window).on('resize', () =>{
+    if(window.innerWidth <= 991){
+        saySlider(1)
+    }
+    else{
+        saySlider(3)
+    }
+})
 
-$(".say_slider").slick({
-    dots: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    adaptiveHeight: true,
-    // autoplay:true,
-    autoplaySpeed:5000,
-    prevArrow: "<img src='img/home/slider_slick_next.svg' class='prev' alt='1'>",
-    nextArrow: "<img src='img/home/slider_slick_next.svg' class='next' alt='2'>",
-});
+
+function saySlider(x) {
+    $(".say_slider").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: x,
+        slidesToScroll: x,
+        adaptiveHeight: true,
+        // autoplay:true,
+        autoplaySpeed:5000,
+        prevArrow: "<img src='img/home/slider_slick_next.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='img/home/slider_slick_next.svg' class='next' alt='2'>",
+    });
+}
