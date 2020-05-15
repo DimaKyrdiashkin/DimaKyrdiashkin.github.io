2ad2f1b6fb0607afddfa7f3efa71559a3b972640
vvalik <ghost0962421083>
2020-05-15 19:27:37 +0300

    15.05.2020 table

--- js/say_slider.js
+++ js/say_slider.js
@@ -1,17 +1,5 @@
 "user script"
 
-//
-// $(".say_slider").slick({
-//     dots: true,
-//     infinite: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 3,
-//     adaptiveHeight: true,
-//     // autoplay:true,
-//     autoplaySpeed:5000,
-//     prevArrow: "<img src='img/home/slider_slick_next.svg' class='prev' alt='1'>",
-//     nextArrow: "<img src='img/home/slider_slick_next.svg' class='next' alt='2'>",
-// });
 if(window.innerWidth <= 991){
     $(".say .say_slider").slick({
         dots: true,
