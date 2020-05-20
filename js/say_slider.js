03b09f123332c56e1c319cd86d85848320174e97
vvalik <ghost0962421083>
2020-05-20 18:53:59 +0300

    slider_index.js

--- js/say_slider.js
+++ js/say_slider.js
@@ -1,66 +1,28 @@
-"user script"
-
-if(window.innerWidth <= 991){
-    $(".say .say_slider").slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        adaptiveHeight: true,
-        // autoplay:true,
-        autoplaySpeed:5000,
-        arrows: false
-    });
-}
-else if(window.innerWidth <= 1200){
-    $(".say .say_slider").slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        adaptiveHeight: true,
-        // autoplay:true,
-        autoplaySpeed:5000,
-        arrows: false
-    });
-}
-else{
-    $(".say .say_slider").slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        adaptiveHeight: true,
-        // autoplay:true,
-        autoplaySpeed:5000,
-        prevArrow: "<img src='img/home/slider_slick_next.svg' class='prev' alt='1'>",
-        nextArrow: "<img src='img/home/slider_slick_next.svg' class='next' alt='2'>",
-    });
-}
-$(window).resize( ()=>{
-    if(window.innerWidth <= 991){
-        $(".say_slider").slick({
-            dots: true,
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            adaptiveHeight: true,
-            // autoplay:true,
-            autoplaySpeed:5000,
-            arrows: false
-        });
-    }
-    else{
-        $(".say .say_slider").slick({
-            dots: true,
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            adaptiveHeight: true,
-            // autoplay:true,
-            autoplaySpeed:5000,
-            prevArrow: "<img src='img/home/slider_slick_next.svg' class='prev' alt='1'>",
-            nextArrow: "<img src='img/home/slider_slick_next.svg' class='next' alt='2'>",
-        });
-    }
-})
+$(".say_slider").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    adaptiveHeight: true,
+    autoplay:true,
+    autoplaySpeed:5000,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: false,
+            }
+        },
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+            }
+        }
+    ]
+});
