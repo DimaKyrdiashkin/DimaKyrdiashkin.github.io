7586fb395dfb522e27a6835793f85df45a218be9
vvalik <ghost0962421083>
2020-05-22 17:49:40 +0300

    22.05.2020 mobile

--- js/NonFullPage.js
+++ js/NonFullPage.js
@@ -61,7 +61,7 @@ function fun_Full() {
         //Design
         controlArrows: true,
         verticalCentered: true,
-        sectionsColor: ['#0000', '#F8F9FF', '#ffffff', '#F8F9FF', '#F8F9FF', '#F8F9FF', '#542AC8','#F8F9FF','#F8F9FF', '#542AC8'],
+        sectionsColor: ['#0000', '#F8F9FF', '#ffffff', '#F8F9FF', '#F8F9FF', '#F8F9FF', 'rgba(0,0,0,0)','#F8F9FF','#F8F9FF', '#542AC8'],
         // paddingTop: '3em',
         // paddingBottom: '10px',
         fixedElements: '#header, #marks, #fp-nav, .marker',
