528e4a33634af35057bba78867adb554896f64b3
DimaKyrdiashkin <44475715+DimaKyrdiashkin@users.noreply.github.com>
2020-06-04 18:06:20 +0300

    04.06.2020

--- js/sollar_slide.js
+++ js/sollar_slide.js
@@ -1,26 +1,13 @@
-// footer_slider
-$("#footer_slider").slick({
-    dots: true,
+// gift_slider
+$("#gift_slider").slick({
+    dots: false,
     infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
     prevArrow: "<img src='img/slider_slick_next.svg' class='prev' alt='1'>",
     nextArrow: "<img src='img/slider_slick_next.svg' class='next' alt='2'>",
     responsive: [
-        {
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-            }
-        },
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-            }
-        },
         {
             breakpoint: 600,
             settings: {
@@ -32,5 +19,10 @@ $("#footer_slider").slick({
         }
     ]
 });
-
-// /footer_slider
+// /gift_slider
+// $('.next').on('click', function(){
+//     $('.next').addClass('click');
+//     setInterval(function(){
+//         $('.next').removeClass('click');
+//     }, 500);
+// });
