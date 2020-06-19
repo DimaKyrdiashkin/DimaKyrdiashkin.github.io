761e4f2d17d3b7b35316f1b65af2b63dc018e91b
DimaKyrdiashkin <44475715+DimaKyrdiashkin@users.noreply.github.com>
2020-06-19 11:56:04 +0300

    19.06.2020

--- js/slider_index.js
+++ js/slider_index.js
@@ -36,35 +36,10 @@ $(".say_slider").slick({
 $("#footer_slider").slick({
     dots: true,
     infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     prevArrow: "<img src='img/slider_slick_next2.svg' class='prev' alt='1'>",
     nextArrow: "<img src='img/slider_slick_next2.svg' class='next' alt='2'>",
-    responsive: [
-        {
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-            }
-        },
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                centerMode: true,
-                dots: false,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        }
-    ]
 });
 
 // /footer_slider
