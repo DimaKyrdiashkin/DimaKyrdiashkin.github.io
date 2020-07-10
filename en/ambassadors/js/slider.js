6f68b7787f20c0541b35838e629b79329f249749
DimaKyrdiashkin <44475715+DimaKyrdiashkin@users.noreply.github.com>
2020-07-10 11:08:26 +0300

    10.07.2020

---
+++ en/ambassadors/js/slider.js
@@ -0,0 +1,13 @@
+$('#slider').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    // autoplay:true,
+    focusOnSelect: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    prevArrow: "<img src='img/slider_slick_next.svg' class='prev' alt='1'>",
+    nextArrow: "<img src='img/slider_slick_next.svg' class='next' alt='2'>",
+})
