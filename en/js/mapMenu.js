1a4bc45508da94180d4a262684a6bce31152ca67
DimaKyrdiashkin <44475715+DimaKyrdiashkin@users.noreply.github.com>
2020-07-10 12:09:38 +0300

    10.07.2020

---
+++ en/js/mapMenu.js
@@ -0,0 +1,62 @@
+$(".menu_button").on("click", openMunu = () => {
+    $(".menu_h1").toggleClass("menu_h1_open");
+
+    if($(".menu").hasClass("opacity")){
+        $('.subM').removeClass('subMenu1_open subMenu2_open subMenu3_open subMenu4_open menuUlActive');
+        $(".side_bar").delay(1000).toggleClass("menu_open", 1000);
+        $(".menu").delay(500).toggleClass("opacity", 500);
+        $('.menu_button').toggleClass('menu_button_active');
+    }else{
+        $(".side_bar").toggleClass("menu_open");
+        $('.menu_button').toggleClass('menu_button_active');
+        $(".menu").delay(1000).toggleClass("opacity", 1200);
+    };
+});
+$(".menu_h1").on("click", () => {
+    openMunu()
+});
+
+$('.subM a').on('click', (e) =>{
+    if(document.documentElement.clientWidth <=480){
+        setTimeout(function(){
+            $('.content').css('opacity', 1);
+        }, 800);
+
+    };
+});
+$('.menu a').on('click', (e) => {
+    const active = e.target.closest('li');
+
+    if(!! active.querySelector('ul.menuUlActive')){
+        $('.menu_a_span_active').removeClass('menu_a_span_active');
+        $('.subM').removeClass('subMenu1_open subMenu2_open subMenu3_open subMenu4_open menuUlActive');
+        return;
+    }
+    $('.menu_a_span_active').removeClass('menu_a_span_active');
+    $('.subM').removeClass('subMenu1_open subMenu2_open subMenu3_open subMenu4_open menuUlActive');
+    if(e.target.hasAttribute('data-point')) {
+        openPoint(e.target.getAttribute('data-point'));
+        openMunu()
+        $(".content-left").addClass("open_content-left", 1000);
+        $(".content-right").addClass("open_content-right", 1000);
+        return
+    }
+    const ulMenu = document.querySelectorAll('.menu>ul>li');
+    let num;
+    for(let i in ulMenu){
+        if(active == ulMenu[i]){
+            num = parseInt(i);
+            break;
+        }
+    }
+    if(!! active.querySelector('a span')){
+        active.querySelector('a').classList.add('menu_a_span_active')
+    }
+    if(num === 0){
+        $(active).children('ul').toggleClass('subMenu1_open menuUlActive');
+    }else if(num === 1){
+        $(active).children('ul').toggleClass('subMenu2_open menuUlActive');
+    }else if(num === 2){
+        $(active).children('ul').toggleClass('subMenu3_open menuUlActive');
+    }
+});
---
+++ en/js/programSlider.js
@@ -0,0 +1,86 @@
+let arr = ['url("../img/event/event1.png"), linear-gradient(0deg, rgba(58, 58, 58, 0.73), rgba(58, 58, 58, 0.73))',
+            'url("../img/event/event2.png"), linear-gradient(0deg, rgba(58, 58, 58, 0.73), rgba(58, 58, 58, 0.73))',
+            'url("../img/event/event3.png"), linear-gradient(0deg, rgba(58, 58, 58, 0.73), rgba(58, 58, 58, 0.73))',
+            'url("../img/event/event4.png"), linear-gradient(0deg, rgba(58, 58, 58, 0.73), rgba(58, 58, 58, 0.73))',
+            'url("../img/event/event5.png"), linear-gradient(0deg, rgba(58, 58, 58, 0.73), rgba(58, 58, 58, 0.73))',
+            'url("../img/event/event6.png"), linear-gradient(0deg, rgba(58, 58, 58, 0.73), rgba(58, 58, 58, 0.73))',
+            'url("../img/event/event7.png"), linear-gradient(0deg, rgba(58, 58, 58, 0.73), rgba(58, 58, 58, 0.73))',
+            'url("../img/event/event8.png"), linear-gradient(0deg, rgba(58, 58, 58, 0.73), rgba(58, 58, 58, 0.73))',
+            'url("../img/event/event9.png"), linear-gradient(0deg, rgba(58, 58, 58, 0.73), rgba(58, 58, 58, 0.73))',
+            'url("../img/event/event10.png"), linear-gradient(0d…, rgba(58, 58, 58, 0.73), rgba(58, 58, 58, 0.73))',
+            'url("../img/event/event11.png"), linear-gradient(0d…, rgba(58, 58, 58, 0.73), rgba(58, 58, 58, 0.73))',
+            'url("../img/event/event12.png"), linear-gradient(0d…, rgba(58, 58, 58, 0.73), rgba(58, 58, 58, 0.73))',
+            'url("../img/event/event13.png"), linear-gradient(0d…, rgba(58, 58, 58, 0.73), rgba(58, 58, 58, 0.73))']
+$('#partner_slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    focusOnSelect: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    prevArrow: "<img src='../img/slider_slick_next.svg' class='prev' alt='1'>",
+    nextArrow: "<img src='../img/slider_slick_next.svg' class='next' alt='2'>",
+    responsive: [
+        {
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: true,
+                dots: false,
+            }
+        },
+        {
+            breakpoint: 481,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                dots: false,
+            }
+        }
+    ]
+})
+//event_slider
+$('#event_slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    focusOnSelect: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    prevArrow: "<img src='../img/slider_slick_next.svg' class='prev' alt='1'>",
+    nextArrow: "<img src='../img/slider_slick_next.svg' class='next' alt='2'>",
+    responsive: [
+        {
+            breakpoint: 993,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: true,
+                dots: false,
+                },         
+        },
+        {
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                dots: false,
+            }
+        },        
+        {
+            breakpoint: 481,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                dots: false,
+            }
+        }        
+    ]
+})
+
