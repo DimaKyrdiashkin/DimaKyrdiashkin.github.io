3168060103f8af9fa7b42e6c864b6b3ed4d1bc45
vvalik <ghost0962421083>
2020-07-15 12:34:19 +0300

    15.07.2020

--- js/menu.js
+++ js/menu.js
@@ -4,11 +4,10 @@ $('.mainSubmenu').on('click', function(){
     $('.mainSubmenu ul').toggleClass('mainSubmenu_open');
 });
 
-$(".menu_button").on("click", function(even){
+$(".menu_button").on("click", function(){
     console.log();
     if(!click){
         $(".menu_button").addClass("isFlip");
-        // $(".menu_button").delay(500).addClass("isFlip_right",1000);
         $(".menu").addClass("menu_open");
         $("header").delay(500).addClass("menu_open", 500);
         $(".menu ul, .menu a, .licens").delay(1000).removeClass("hide", 500);
