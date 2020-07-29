63c334a78fddb9aeff9ea1214bf4e2935e8cb798
VVALIK <valikpost8@gmail.com>
2020-07-29 16:06:05 +0300

    29.07.2020 en city

--- js/mapMap.js
+++ js/mapMap.js
@@ -1,35 +1,34 @@
-window.onload=function(){
+window.onload = function () {
     setTimeout(
-        ()=>{
+        () => {
             document.querySelector(".loader_active svg").style.opacity = 0;
-        },1000
+        }, 1000
     )
     setTimeout(
-        ()=>{
-
+        () => {
             document.querySelector(".loader_active").classList.remove('loader_active');
-        },1500
+        }, 1500
     )
     //
-    setTimeout(()=>{
+    setTimeout(() => {
         document.querySelector(".loader").style.display = 'none'
-    },2000)
+    }, 2000)
 }
 
 let mousePosition,
-    offset = [0,0],
+    offset = [0, 0],
     isDown = false,
     scaleVal2 = 1,
     scaleVal = 0,
-    sityW = $(".city").width()/200,
-    sityH = $(".city").height()/200,
+    sityW = $(".city").width() / 200,
+    sityH = $(".city").height() / 200,
     div = document.querySelector(".city"),
     widthImg = -3200,
     heightImg = -1800;
 
 let city = document.querySelector(".city");
 
-div.addEventListener('mousedown', (e) =>{
+div.addEventListener('mousedown', (e) => {
     div.style.transition = "0s";
     div.style.cursor = "grabbing";
     isDown = true;
@@ -39,7 +38,7 @@ div.addEventListener('mousedown', (e) =>{
     ];
 }, true);
 
-div.addEventListener('touchstart', (e) =>{
+div.addEventListener('touchstart', (e) => {
     div.style.transition = "0s";
     div.style.cursor = "grabbing";
     isDown = true;
@@ -49,65 +48,69 @@ div.addEventListener('touchstart', (e) =>{
     ];
 }, true);
 
-document.addEventListener('mousemove', (e) =>{
+document.addEventListener('mousemove', (e) => {
     e.preventDefault();
     if (isDown) {
         mousePosition = {
-            x : e.clientX,
-            y : e.clientY
+            x: e.clientX,
+            y: e.clientY
 
         };
         div.style.left = (mousePosition.x + offset[0]) + 'px';
-        div.style.top  = (mousePosition.y + offset[1]) + 'px';
+        div.style.top = (mousePosition.y + offset[1]) + 'px';
     }
 }, true);
 
-document.addEventListener('touchmove', (e) =>{
+document.addEventListener('touchmove', (e) => {
     // e.preventDefault();
     if (isDown) {
         mousePosition = {
-            x : e.touches[0].pageX,
-            y : e.touches[0].pageY
+            x: e.touches[0].pageX,
+            y: e.touches[0].pageY
 
         };
         div.style.left = (mousePosition.x + offset[0]) + 'px';
-        div.style.top  = (mousePosition.y + offset[1]) + 'px';
+        div.style.top = (mousePosition.y + offset[1]) + 'px';
     }
 }, true);
 
-document.addEventListener('mouseup', ()=> {
+document.addEventListener('mouseup', () => {
     isDown = false;
     div.style.transition = "1s";
-    if(city.offsetTop > sityH*parseInt(scaleVal*100)){
-        div.style.top = sityH*parseInt(scaleVal*100)+"px";
+    if (city.offsetTop > sityH * parseInt(scaleVal * 100)) {
+        div.style.top = sityH * parseInt(scaleVal * 100) + "px";
     }
-    if (city.offsetLeft > sityW*parseInt(scaleVal*100)+64){
-        div.style.left = sityW*parseInt(scaleVal*100)+64+"px";
+    if (city.offsetLeft > sityW * parseInt(scaleVal * 100) + 64) {
+        div.style.left = sityW * parseInt(scaleVal * 100) + 64 + "px";
     }
-    if(city.offsetTop < (heightImg+document.documentElement.clientHeight)-sityH*parseInt(scaleVal*100)){
-        div.style.top = (heightImg+document.documentElement.clientHeight)-sityH*parseInt(scaleVal*100)+"px";
-    };
-    if(city.offsetLeft < (widthImg+document.documentElement.clientWidth)-sityW*parseInt(scaleVal*100)) {
-        div.style.left = (widthImg+document.documentElement.clientWidth)-sityW*parseInt(scaleVal*100)+"px";
-    };
+    if (city.offsetTop < (heightImg + document.documentElement.clientHeight) - sityH * parseInt(scaleVal * 100)) {
+        div.style.top = (heightImg + document.documentElement.clientHeight) - sityH * parseInt(scaleVal * 100) + "px";
+    }
+    ;
+    if (city.offsetLeft < (widthImg + document.documentElement.clientWidth) - sityW * parseInt(scaleVal * 100)) {
+        div.style.left = (widthImg + document.documentElement.clientWidth) - sityW * parseInt(scaleVal * 100) + "px";
+    }
+    ;
     div.style.cursor = "grab";
 }, true);
 
-document.addEventListener('touchend', ()=> {
+document.addEventListener('touchend', () => {
     isDown = false;
     div.style.transition = "1s";
-    if(city.offsetTop > sityH*parseInt(scaleVal*100)){
-        div.style.top = sityH*parseInt(scaleVal*100)+"px";
+    if (city.offsetTop > sityH * parseInt(scaleVal * 100)) {
+        div.style.top = sityH * parseInt(scaleVal * 100) + "px";
     }
-    if (city.offsetLeft > sityW*parseInt(scaleVal*100)+64){
-        div.style.left = sityW*parseInt(scaleVal*100)+64+"px";
+    if (city.offsetLeft > sityW * parseInt(scaleVal * 100) + 64) {
+        div.style.left = sityW * parseInt(scaleVal * 100) + 64 + "px";
     }
-    if(city.offsetTop < (heightImg+document.documentElement.clientHeight)-sityH*parseInt(scaleVal*100)){
-        div.style.top = (heightImg+document.documentElement.clientHeight)-sityH*parseInt(scaleVal*100)+"px";
-    };
-    if(city.offsetLeft < (widthImg+document.documentElement.clientWidth)-sityW*parseInt(scaleVal*100)) {
-        div.style.left = (widthImg+document.documentElement.clientWidth)-sityW*parseInt(scaleVal*100)+"px";
-    };
+    if (city.offsetTop < (heightImg + document.documentElement.clientHeight) - sityH * parseInt(scaleVal * 100)) {
+        div.style.top = (heightImg + document.documentElement.clientHeight) - sityH * parseInt(scaleVal * 100) + "px";
+    }
+    ;
+    if (city.offsetLeft < (widthImg + document.documentElement.clientWidth) - sityW * parseInt(scaleVal * 100)) {
+        div.style.left = (widthImg + document.documentElement.clientWidth) - sityW * parseInt(scaleVal * 100) + "px";
+    }
+    ;
     div.style.cursor = "grab";
 }, true);
 
@@ -117,50 +120,53 @@ $('.city').bind('mousewheel', (e) => {
             scaleVal += 0.01;
             scaleVal2 += 0.01;
             city.style.transform = `scale(${scaleVal2})`
-        }else{
-            window.addEventListener('touchstart', e => e.preventDefault(), { passive: false });
-        };
-    } else{
+        } else {
+            window.addEventListener('touchstart', e => e.preventDefault(), {passive: false});
+        }
+        ;
+    } else {
         if (scaleVal2 >= 1.01) {
-            scaleVal-=0.01;
-            scaleVal2-=0.01;
+            scaleVal -= 0.01;
+            scaleVal2 -= 0.01;
             city.style.transform = `scale(${scaleVal2})`
-            if(city.offsetTop > sityH*parseInt(scaleVal*100)){
-                div.style.top = sityH*parseInt(scaleVal*100)+"px";
-            }
-            if (city.offsetLeft > sityW * parseInt(scaleVal * 100) + 64){
-                div.style.left = sityW * parseInt(scaleVal * 100) + 64 + "px";
-            }
-            if(city.offsetTop < (heightImg+document.documentElement.clientHeight) - sityH * parseInt(scaleVal * 100)){
-                div.style.top = (heightImg+document.documentElement.clientHeight) - sityH * parseInt(scaleVal * 100) + "px";
-            }
-            if(city.offsetLeft < (widthImg+document.documentElement.clientWidth) - sityW * parseInt(scaleVal * 100)) {
-                div.style.left = (widthImg+document.documentElement.clientWidth) - sityW * parseInt(scaleVal * 100) + "px";
-            }
-        }else{
-            window.addEventListener('touchstart', e => e.preventDefault(), { passive: false });
-            scaleVal = 0;
-            if(city.offsetTop > sityH * parseInt(scaleVal * 100)){
+            if (city.offsetTop > sityH * parseInt(scaleVal * 100)) {
                 div.style.top = sityH * parseInt(scaleVal * 100) + "px";
             }
-            if(city.offsetLeft > sityW * parseInt(scaleVal * 100) + 64){
+            if (city.offsetLeft > sityW * parseInt(scaleVal * 100) + 64) {
                 div.style.left = sityW * parseInt(scaleVal * 100) + 64 + "px";
             }
-            if(city.offsetTop < (-1898+document.documentElement.clientHeight) - sityH * parseInt(scaleVal * 100)){
-                div.style.top = (heightImg+document.documentElement.clientHeight) - sityH * parseInt(scaleVal * 100)+"px";
+            if (city.offsetTop < (heightImg + document.documentElement.clientHeight) - sityH * parseInt(scaleVal * 100)) {
+                div.style.top = (heightImg + document.documentElement.clientHeight) - sityH * parseInt(scaleVal * 100) + "px";
             }
-            if(city.offsetLeft < (widthImg+document.documentElement.clientWidth) - sityW * parseInt(scaleVal * 100)) {
-                div.style.left = (widthImg+document.documentElement.clientWidth) - sityW * parseInt(scaleVal * 100)+"px";
-            };
-        };
-    };
+            if (city.offsetLeft < (widthImg + document.documentElement.clientWidth) - sityW * parseInt(scaleVal * 100)) {
+                div.style.left = (widthImg + document.documentElement.clientWidth) - sityW * parseInt(scaleVal * 100) + "px";
+            }
+        } else {
+            window.addEventListener('touchstart', e => e.preventDefault(), {passive: false});
+            scaleVal = 0;
+            if (city.offsetTop > sityH * parseInt(scaleVal * 100)) {
+                div.style.top = sityH * parseInt(scaleVal * 100) + "px";
+            }
+            if (city.offsetLeft > sityW * parseInt(scaleVal * 100) + 64) {
+                div.style.left = sityW * parseInt(scaleVal * 100) + 64 + "px";
+            }
+            if (city.offsetTop < (-1898 + document.documentElement.clientHeight) - sityH * parseInt(scaleVal * 100)) {
+                div.style.top = (heightImg + document.documentElement.clientHeight) - sityH * parseInt(scaleVal * 100) + "px";
+            }
+            if (city.offsetLeft < (widthImg + document.documentElement.clientWidth) - sityW * parseInt(scaleVal * 100)) {
+                div.style.left = (widthImg + document.documentElement.clientWidth) - sityW * parseInt(scaleVal * 100) + "px";
+            }
+            ;
+        }
+        ;
+    }
+    ;
 });
 
-$(".point, .close_cross").on("click",  () => {
-    if(document.querySelector('.content').style.opacity && document.documentElement.clientWidth <= 480){
+$(".point, .close_cross").on("click", () => {
+    if (document.querySelector('.content').style.opacity && document.documentElement.clientWidth <= 480) {
         document.querySelector('.content').style.opacity = 0;
-    }
-    else{
+    } else {
         document.querySelector('.content').style.opacity = 1;
     }
     $(".content-left").toggleClass("open_content-left", 500);
@@ -172,53 +178,51 @@ let mapBD,
     numberPointActive = 1;
 $.ajax({
     url: "../js/mapBd.json"
-}).done(function(data) {
-    mapBD=data;
+}).done(function (data) {
+    mapBD = data;
 })
-const  fatherBlock = document.getElementById("father"),
+const fatherBlock = document.getElementById("father"),
     titlePointBlock = document.getElementById('titlePoint'),
     textHomePointBlock = document.getElementById('textHomePoint'),
     logoPointerBlock = document.getElementById('logoPointer')
 
-$(".point").on("click",(e)=>{
+$(".point").on("click", (e) => {
     const numberPoint = parseInt(e.target.closest('.point').classList[1].slice(1));
     console.log(numberPoint);
-    
+
     openPoint(numberPoint)
 });
 
 const openPoint = (numberPoint) => {
-    let father ='';
+    let father = '';
     numberPointActive = numberPoint;
 
-    if(28 < numberPoint) numberPoint=1
-    else if(1>numberPoint) numberPoint= 28
+    if (28 < numberPoint) numberPoint = 1
+    else if (1 > numberPoint) numberPoint = 28
     const BdName = `p${numberPoint}`;
-    if(numberPoint<21){
-        father = "Экосистема Amir Capital";
-    }
-    else if(numberPoint<23){
-        father = "Cообщество Amir Partners (AmirID)";
-    }
-    else if(numberPoint<29){
+    if (numberPoint < 21) {
+        father = "Amir Capital ecosystem";
+    } else if (numberPoint < 23) {
+        father = "Amir Partners Community (AmirID)";
+    } else if (numberPoint < 29) {
         father = "Social Evolution Company";
     }
     fatherBlock.innerHTML = father;
     titlePointBlock.innerHTML = mapBD[BdName].title;
     textHomePointBlock.innerHTML = mapBD[BdName].text;
-    logoPointerBlock.src = "./img/logoPoint/"+mapBD[BdName].imgUrl;
+    logoPointerBlock.src = "./img/logoPoint/" + mapBD[BdName].imgUrl;
 }
 
-document.getElementById('nextPoint').addEventListener('click', ()=>{
+document.getElementById('nextPoint').addEventListener('click', () => {
     const textActive = titlePointBlock.innerHTML
-    for(let i in mapBD){
-        if(mapBD[i].title == textActive)openPoint(parseInt(i.split('p')[1])+1)
+    for (let i in mapBD) {
+        if (mapBD[i].title == textActive) openPoint(parseInt(i.split('p')[1]) + 1)
     }
 })
-document.getElementById('poverPoint').addEventListener('click', ()=>{
+document.getElementById('poverPoint').addEventListener('click', () => {
     const textActive = titlePointBlock.innerHTML
-    for(let i in mapBD){
-        if(mapBD[i].title == textActive)openPoint(parseInt(i.split('p')[1])-1)
+    for (let i in mapBD) {
+        if (mapBD[i].title == textActive) openPoint(parseInt(i.split('p')[1]) - 1)
     }
 })
 
@@ -226,37 +230,36 @@ document.getElementById('poverPoint').addEventListener('click', ()=>{
 
 w_content_right_top = document.getElementById('content_right_top').clientWidth;
 h_content_right_top = document.getElementById('content_right_top').clientHeight;
-const  svg= document.querySelectorAll('.content-right_top_svg')
+const svg = document.querySelectorAll('.content-right_top_svg')
 
 setInterval(
-    ()=>{
-        for(let i of svg){
-            i.style.top = `${(Math.random()*h_content_right_top)}px`;
-            i.style.left = `${(Math.random()*h_content_right_top)}px`;
+    () => {
+        for (let i of svg) {
+            i.style.top = `${(Math.random() * h_content_right_top)}px`;
+            i.style.left = `${(Math.random() * h_content_right_top)}px`;
         }
-    },2000
+    }, 2000
 )
 
 const localPoint = localStorage.point,
-    localPointFun = (name)=>{
+    localPointFun = (name) => {
 
-    for(let i in mapBD){
-        if(mapBD[i].title === name){
-            if(document.querySelector('.content').style.opacity && document.documentElement.clientWidth <= 480){
-                document.querySelector('.content').style.opacity = 0;
+        for (let i in mapBD) {
+            if (mapBD[i].title === name) {
+                if (document.querySelector('.content').style.opacity && document.documentElement.clientWidth <= 480) {
+                    document.querySelector('.content').style.opacity = 0;
+                } else {
+                    document.querySelector('.content').style.opacity = 1;
+                }
+                $(".content-left").toggleClass("open_content-left", 500);
+                $(".content-right").toggleClass("open_content-right", 500);
+                let num = parseInt(i.match(/\d+/))
+                openPoint(num)
             }
-            else{
-                document.querySelector('.content').style.opacity = 1;
-            }
-            $(".content-left").toggleClass("open_content-left", 500);
-            $(".content-right").toggleClass("open_content-right", 500);
-            let num = parseInt(i.match(/\d+/))
-            openPoint(num)
         }
     }
-}
-setTimeout(()=>{
-    if(!! localPoint){
+setTimeout(() => {
+    if (!!localPoint) {
         localPointFun(localPoint)
     }
 }, 1000)
