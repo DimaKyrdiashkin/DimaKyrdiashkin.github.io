8cf501dd0d30320dcebf185bc75313c7007220c3
DimaKyrdiashkin <44475715+DimaKyrdiashkin@users.noreply.github.com>
2020-08-21 15:38:58 +0300

    21.08.2020

--- static/js/main.js
+++ static/js/main.js
@@ -1,56 +1,59 @@
-const card =  document.querySelectorAll('.cardT');
-const cardHeight = [];
-const minH = [];
+// const card =  document.querySelectorAll('.cardT');
+// const cardHeight = [];
+// const minH = [];
 
-card.forEach((item)=>{
-    cardHeight.push(item.clientHeight + 2)
-});
-// cut text
-const textFull = document.querySelectorAll('.cardT');
-const button = document.querySelectorAll('.card_button');
-let textFullText= [];
+// card.forEach((item)=>{
+//     cardHeight.push(item.clientHeight + 2)
+// });
+// // cut text
+// const textFull = document.querySelectorAll('.cardT');
+// const button = document.querySelectorAll('.card_button');
+// let textFullText= [];
 
 
-textFull.forEach((item)=>{
-    textFullText.push(item.innerText)
-});
+// textFull.forEach((item)=>{
+//     textFullText.push(item.innerText)
+// });
 
-const funSub = (i)=>{
-    document.querySelectorAll('.cardT')[i].innerText = textFull[i].innerText.substr(0, 150)+'...';
-}
+// const funSub = (i)=>{
+//     document.querySelectorAll('.cardT')[i].innerText = textFull[i].innerText.substr(0, 150)+'...';
+// }
 
-textFull.forEach((item, i)=>{
-    funSub(i)
-});
+// textFull.forEach((item, i)=>{
+//     funSub(i)
+// });
 
-card.forEach((item, i)=>{
-    minH.push(item.clientHeight + 2);
-    item.style.height = `${minH[i]}px`;
-});
+// card.forEach((item, i)=>{
+//     minH.push(item.clientHeight + 2);
+//     item.style.height = `${minH[i]}px`;
+// });
 
-const short = (i)=>{
-    textFull.forEach((item, i)=>{
-        card[i].style.height = `${minH[i]}px`;
-        funSub(i)
-        button[i].classList.remove("open");
-    });
-};
+// const short = (i)=>{
+//     textFull.forEach((item, i)=>{
+//         card[i].style.height = `${minH[i]}px`;
+//         funSub(i)
+//         button[i].classList.remove("open");
+//     });
+// };
 
-const open=(i)=>{
-    if(textFull[i].innerText.length >153){
-        card[i].style.height = `${minH[i]}px`;
-        funSub(i);
-        button[i].classList.remove("open");
-        return false;
-    }
-    short(i);
-    card[i].style.height = `${cardHeight[i]}px`;
-    document.querySelectorAll('.cardT')[i].innerText = textFullText[i];
-    button[i].classList.add("open");
-}
-button.forEach((item, i)=>{
-    item.addEventListener("click", ()=>{open(i)});
-});
+// const open=(i)=>{
+//     if(textFull[i].innerText.length >153){
+//         card[i].style.height = `${minH[i]}px`;
+//         funSub(i);
+//         button[i].classList.remove("open");
+//         return false;
+//     }
+//     short(i);
+//     card[i].style.height = `${cardHeight[i]}px`;
+//     document.querySelectorAll('.cardT')[i].innerText = textFullText[i];
+//     button[i].classList.add("open");
+// }
+// button.forEach((item, i)=>{
+//     item.addEventListener("click", ()=>{open(i)});
+// });
+$('.card_button').click(function(){
+    $(this).parent().find('.hidden').slideToggle(300);
+})
 // menu
 const mBut = document.querySelector('.mobButton');
 const menu = document.querySelector('.mobMenu');
