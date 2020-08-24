fd369d4105e4bc9bdfa5220a7eae7a57860e2305
DimaKyrdiashkin <44475715+DimaKyrdiashkin@users.noreply.github.com>
2020-08-24 11:40:47 +0300

    24.08.2020

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,14 @@
-// const card =  document.querySelectorAll('.cardT');
-// const cardHeight = [];
-// const minH = [];
+const card =  document.querySelectorAll('.cardT');
+const cardHeight = [];
+const minH = [];
 
-// card.forEach((item)=>{
-//     cardHeight.push(item.clientHeight + 2)
-// });
+card.forEach((item)=>{
+    cardHeight.push(item.clientHeight + 2)
+});
+document.querySelector('.cardT').style.maxHeight = "76px";
 // // cut text
 // const textFull = document.querySelectorAll('.cardT');
-// const button = document.querySelectorAll('.card_button');
+const button = document.querySelectorAll('.card_button');
 // let textFullText= [];
 
 
@@ -36,30 +37,42 @@
 //     });
 // };
 
-// const open=(i)=>{
-//     if(textFull[i].innerText.length >153){
-//         card[i].style.height = `${minH[i]}px`;
-//         funSub(i);
-//         button[i].classList.remove("open");
-//         return false;
-//     }
-//     short(i);
-//     card[i].style.height = `${cardHeight[i]}px`;
-//     document.querySelectorAll('.cardT')[i].innerText = textFullText[i];
-//     button[i].classList.add("open");
-// }
-// button.forEach((item, i)=>{
-//     item.addEventListener("click", ()=>{open(i)});
-// });
-$('.card_button').click(function(){
-    $(this).toggleClass('open');
-    $('.hidden').slideUp(900);
-    if($(this).hasClass('open')){
-        $(this).parent().find('.hidden').slideToggle(900);
-    }else{
+const open=(i)=>{
+    // document.querySelector('.card_button').className.split(' ')[1]
+    if(button[i].className.split(' ')[1] == 'open'){
+        card[i].style.maxHeight = '76px';
+        button[i].classList.remove("open");
         return false
+    }else{
+        card[i].style.maxHeight = `${cardHeight[i]}px`;
     }
-})
+    // if(textFull[i].innerText.length >153){
+    //     card[i].style.height = `${minH[i]}px`;
+    //     funSub(i);
+    //     button[i].classList.remove("open");
+    //     return false;
+    // }
+    // short(i);
+    
+    // document.querySelectorAll('.cardT')[i].innerText = textFullText[i];
+    button[i].classList.add("open");
+}
+button.forEach((item, i)=>{
+    item.addEventListener("click", ()=>{open(i)});
+});
+
+
+// $('.card_button').click(function(){
+//     $(this).toggleClass('open');
+//     $('.hidden').slideUp(900);
+//     if($(this).hasClass('open')){
+//         $(this).parent().find('.hidden').slideToggle(900);
+//     }else{
+//         return false
+//     }
+// })
+
+
 // menu
 const mBut = document.querySelector('.mobButton');
 const menu = document.querySelector('.mobMenu');
